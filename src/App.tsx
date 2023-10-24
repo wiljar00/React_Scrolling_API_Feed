d2b870f82e216b213d6682956c9c93bcd3db5734
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-10-23 22:04:53 -0600

    Created scrollable page.
    Using a sample api, the main app now shows a scrollable list of items. Next step is to add css and implement card components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState<any[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('https://api.sampleapis.com/coffee/hot');
+      setData((prevData) => [...prevData, ...response.data]);
+      setPage(page + 1);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+      ) {
+        fetchData();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    {data.map((item) => (
+      <div key={item.id}>
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+        <p>Ingredients: {item.ingredients.join(", ")}</p>
+        <img src={item.image} alt={item.title} style={{ width: '200px', height: '200px' }} />
+      </div>
+    ))}
+    {loading && <div>Loading...</div>}
+  </div>
   );
 }
 
