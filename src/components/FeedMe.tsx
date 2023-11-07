83e9db0e7cd46f233edb8362c072f419cf64b1ef
Jarom Wilcox <wiljar00@users.noreply.github.com>
2023-11-06 23:25:13 -0700

    CSS Updates
    Playing around with CSS animations. Button has additional effects now.

--- src/components/FeedMe.tsx
+++ src/components/FeedMe.tsx
@@ -2,21 +2,31 @@ import React from 'react';
 import CardList from './CardList';
 
 type FeedMeProps = {
-  getRandomItem: () => void;
-  randomItem: any;
-};
+    getRandomItem: () => void;
+    randomItem: any;
+  };
 
 const FeedMe: React.FC<FeedMeProps> = ({ getRandomItem, randomItem }) => {
-  return (
+    const handleClick = () => {
+        getRandomItem();
+        const container = document.querySelector('.random-item-container');
+        if (container) {
+          container.classList.add('active');
+        }
+      };
+  
+    return (
     <div className="feed-me-container">
-      <h2 className="feed-me-title">Can't Decide? Let me pick something for you!</h2>
-      <button className="btn btn-primary mt-3" onClick={getRandomItem}>
+      <h2 className="feed-me-title">Can't Decide? Let me pick for you!</h2>
+      <button className="btn btn-primary mt-3" onClick={handleClick}>
         Get Recommendation
       </button>
       {randomItem && (
-        <div className="random-item-container">
+        <div className={`random-item-container active`}>
           <h3 className="random-item-title">How about this?</h3>
-          <CardList data={[randomItem]} loading={false} />
+          {randomItem.image && ( // Add a conditional check here
+            <CardList data={[randomItem]} loading={false} />
+          )}
         </div>
       )}
     </div>
